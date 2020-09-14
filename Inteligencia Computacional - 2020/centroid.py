787b2e788cbe8eb0afbec4eecc7e12bee681a9ef
lucasmenescal <menes_pontes@hotmail.com>
2020-09-14 16:40:54 -0300

    .

--- Inteligencia Computacional - 2020/centroid.py
+++ Inteligencia Computacional - 2020/centroid.py
@@ -1,5 +1,28 @@
+import numpy as np
 import pandas as pd
-iris = pd.read_csv("iris.csv")
-iris.head()
+from matplotlib import pyplot as plt
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.cluster import KMeans
+#Gerar os dados que serão usados usando o make-blobs dp sklearn
+#centers = numero de clusters
+X, y = make_blobs(n_samples=300, centers=4, cluster_std=0.60, random_state=0)
+plt.scatter(X[:,0], X[:,1])
 
-X = iris.iloc[:, 0:4].values
+#Metodo de Cotovelo(Elbow method), para determinar a melhor quantidade de clusters.
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 11), wcss)
+plt.title('Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+#plt.show()
+
+#Executando
+kmeans = KMeans(n_clusters=int(input("Digite o numero de clusters: ")), init='k-means++', max_iter=300, n_init=10, random_state=0)
+pred_y = kmeans.fit_predict(X)
+plt.scatter(X[:,0], X[:,1])
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='red')
+plt.show()
